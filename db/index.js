af9958181e829572e767e62664116285709a5de5
aizealawin <ktmudalige@gmail.com>
2022-11-01 14:40:36 -0400

    heroku pls 3

--- db/index.js
+++ db/index.js
@@ -5,9 +5,11 @@ let dbURL =
   process.env.NODE_env === 'production'
     ? process.env.MONGODB_URI
     : 'mongodb://127.0.0.1:27017/TCTPFE'
-
+console.log(process.env.MONGODB_URI)
 mongoose
-  .connect(dbURL)
+  .connect(
+    'mongodb+srv://TCTPFE:123@tctpfe.6gop5pn.mongodb.net/?retryWrites=true&w=majority'
+  )
   .then(() => {
     console.log('Successfully connected to MongoDB.')
   })
