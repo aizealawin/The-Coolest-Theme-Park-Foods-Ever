ccebe4bd9bd1f17537c80dce9253db18c26bd36f
LaShelle Rauseo <lrauseo3@gmail.com>
2022-11-01 15:56:45 -0500

    styling pages

--- client/src/components/MenuForm.jsx
+++ client/src/components/MenuForm.jsx
@@ -33,11 +33,11 @@
         <input id="name" value={formState.name} onChange={handleChange} />
         <label htmlFor="description">Description:</label>
         <input id="description" value={formState.description} onChange={handleChange}/>
-        <label htmlFor="image">Image</label>
+        <label htmlFor="image">Image:</label>
         <input id="image" value={formState.image} onChange={handleChange}/>
-        <label htmlFor="retail_price">Retail Price</label>
+        <label htmlFor="retail_price">Retail Price:</label>
         <input id="retail_price" value={formState.retail_price} onChange={handleChange}/>
-        <label htmlFor="vendor_price">Vendor Price</label>
+        <label htmlFor="vendor_price">Vendor Price:</label>
         <input id="vendor_price" value={formState.vendor_price} onChange={handleChange}/>
         <button type="submit" >Add Food Item</button>
 
